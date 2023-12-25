038a984cd81f7228511c1c9c24c6e2cba5908ca7
Anthony <anthonyhermanto1@gmail.com>
2023-12-25 21:40:09 +0800

    initial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+
+
+
+export default function Home() {
+    return <h1>Fintech lets go</h1>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,11 +9,25 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    fontFamily: {
+      'Roboto': ['Roboto', 'sans-serif'],
+    },
+    fontSize : {
+      xs: "0.6rem",
+      sm: '0.8rem',
+      lg: "1.10rem",
+      md: "0.9rem",
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': "4rem"
     },
   },
   plugins: [],
 }
+
