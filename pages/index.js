04bac717aa3ba831b8f89697b5a31a9a4e8d2449
Anthony <anthonyhermanto1@gmail.com>
2024-01-05 10:48:18 +0800

    added the aelf api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Button from '@mui/material/Button';
+import AElf from 'aelf-sdk';
 
 export default function Home() {
+    const aelf = new AElf(new AElf.providers.HttpProvider('http://127.0.0.1:1235'));
+    console.log(aelf);
     return (
         <div>
             <h1>Hello</h1>
-            <Button variant="contained">Hello world</Button>;
+            <Button variant="contained" onClick = {() => {console.log("hello world")}}>Hello world</Button>;
         </div>
     )
 }
