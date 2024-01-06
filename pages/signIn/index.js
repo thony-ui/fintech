ef4c3a9143828c124175772d6e1d66b4de94bd11
Shelia Teo <teo.zihui.shelia@gmail.com>
2024-01-06 15:17:53 +0800

    sign up page completed

---
+++ pages/signIn/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../../components/Form';
+
+const index = () => {
+  return (
+    <div>
+        Hello Jun Yu
+        <Form />
+    </div>
+  )
+}
+
+export default index
