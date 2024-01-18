19c3283dc5cf3ed99ba737fee294be71c0cadccb
Anthony <anthonyhermanto1@gmail.com>
2024-01-18 11:49:01 +0800

    lol

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
-    const chain = req.id
-    const apiUrl = `https://explorer-test-side02.aelf.io/api/viewer/all/tokenTxList?symbol=${chain}&pageSize=50&pageNum=1`;
+    const { id } = req.query
+    const apiUrl = `https://explorer-test-side02.aelf.io/api/viewer/all/tokenTxList?symbol=${id}&pageSize=50&pageNum=1`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     res.status(200).json(data);
